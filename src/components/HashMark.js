a49784e43de87dc37feb6a8bccea007664faf5be
Rick <beyer.richard73@gmail.com>
2021-08-25 10:15:17 -0500

    CHanged width of hashmarks to an even number so second hand apears directly in the middle

--- src/components/HashMark.js
+++ src/components/HashMark.js
@@ -2,7 +2,7 @@ import './HashMark.css';
 
 const HashMark = (props) => {
     return (
-        <div className='hash-mark' style={{transform: `rotate(-${props.angle}deg)`, height: props.size % 5 === 0 ? '10px' : '5px', width: props.size % 5 === 0 ? '7px' : '3px'}}>
+        <div className='hash-mark' style={{transform: `rotate(-${props.angle}deg)`, height: props.size % 5 === 0 ? '10px' : '5px', width: props.size % 5 === 0 ? '8px' : '4px'}}>
             
         </div>
     )
